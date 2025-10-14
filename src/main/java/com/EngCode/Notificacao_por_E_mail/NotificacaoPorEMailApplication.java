41b4299bf2b413ba78fe5668fe008959b82cbe01
Iarlley <iarlleymangueira@gmail.com>
2025-10-14 14:25:16 -0300

    Primeiro commit

---
+++ src/main/java/com/EngCode/Notificacao_por_E_mail/NotificacaoPorEMailApplication.java
@@ -0,0 +1,13 @@
+package com.EngCode.Notificacao_por_E_mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacaoPorEMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacaoPorEMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EngCode/Notificacao_por_E_mail/NotificacaoPorEMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EngCode.Notificacao_por_E_mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacaoPorEMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
